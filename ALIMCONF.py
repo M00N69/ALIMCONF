d7f3638904816b96dcdc6730b0653e3997aefd12
MOON <133333114+M00N69@users.noreply.github.com>
2024-08-31 18:34:41 +0200

    Update ALIMCONF.py

--- ALIMCONF.py
+++ ALIMCONF.py
@@ -3,57 +3,112 @@ import pandas as pd
 import requests
 import folium
 from streamlit_folium import folium_static
-import plotly.express as px
-
-# Application de styles CSS personnalisés
-st.markdown(
-    """
-    <style>
-    .stButton>button {
-        background-color: #4CAF50;
-        color: white;
-        border-radius: 8px;
-    }
-    .stTextInput>div>div>input {
-        border: 2px solid #4CAF50;
-        border-radius: 4px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 # Fonction pour récupérer les données de l'API
 def get_data_from_api(year, month=None):
-    url = f"https://dgal.opendatasoft.com/api/explore/v2.1/catalog/datasets/export_alimconfiance/records?limit=100&refine=date_inspection%3A%22{year}%22"
+    url = f"https://dgal.opendatasoft.com/api/explore/v2.1/catalog/datasets/export_alimconfiance/records?refine=date_inspection%3A%22{year}%22"
     if month is not None:
         url += f"&refine=date_inspection%3A%22{year}-{month:02}%22"
     response = requests.get(url)
     data = response.json()
-    
+
     if 'results' in data:
         df = pd.DataFrame(data['results'])
     else:
         st.error("Erreur : format de réponse JSON incorrect.")
         return None
 
-    df['date_inspection'] = pd.to_datetime(df['date_inspection'])
+    if 'date_inspection' in df.columns:
+        df['date_inspection'] = pd.to_datetime(df['date_inspection'])
+    else:
+        st.error("Erreur : la colonne 'date_inspection' est manquante.")
+        return None
+
     return df
 
-# Fonction pour créer la carte interactive
-def create_map(df):
-    map_center = [46.2276, 2.2137]
-    map = folium.Map(location=map_center, zoom_start=6)
+# Interface utilisateur Streamlit
+st.title('Données AlimConfiance')
 
+# Slider pour sélectionner l'année et le mois
+start_year, end_year = st.sidebar.slider(
+    "Sélectionnez la période d'inspection (année)",
+    min_value=2015,
+    max_value=2024,
+    value=(2023, 2024),
+    step=1
+)
+
+# Récupérer les données de l'API pour chaque année de la période
+dfs = []
+for year in range(start_year, end_year + 1):
+    df = get_data_from_api(year)
+    if df is not None:
+        dfs.append(df)
+
+# Concatenate data for all selected years
+if dfs:
+    df = pd.concat(dfs, ignore_index=True)
+else:
+    st.error("Aucune donnée disponible pour la période sélectionnée.")
+    df = None
+
+if df is not None:
+    # Filtrage
+    st.sidebar.title('Filtrage')
+
+    all_levels = ['Tous', 'Très satisfaisant', 'Satisfaisant', 'A améliorer', 'A corriger de manière urgente']
+    niveau_resultat = st.sidebar.selectbox("Niveau de résultat", all_levels)
+
+    activite_etablissement_unique = set()
+    for activites in df['app_libelle_activite_etablissement']:
+        activite_etablissement_unique.update(activites)
+    activite_etablissement = st.sidebar.multiselect(
+        "Activité de l'établissement", sorted(activite_etablissement_unique)
+    )
+
+    filtre_categorie_unique = set()
+    for filtres in df['filtre']:
+        if isinstance(filtres, list):
+            filtre_categorie_unique.update(filtres)
+    filtre_categorie = st.sidebar.multiselect(
+        "Catégorie de filtre", sorted(filtre_categorie_unique)
+    )
+
+    ods_type_activite = st.sidebar.multiselect(
+        "Type d'activité", sorted(df['ods_type_activite'].unique())
+    )
+
+    nom_etablissement = st.sidebar.text_input("Nom de l'établissement")
+    adresse = st.sidebar.text_input("Adresse")
+
+    # Appliquer les filtres
+    if niveau_resultat != 'Tous':
+        df = df[df['synthese_eval_sanit'] == niveau_resultat]
+
+    if activite_etablissement:
+        df = df[df['app_libelle_activite_etablissement'].apply(lambda x: any(item in x for item in activite_etablissement))]
+
+    if filtre_categorie:
+        df = df[df['filtre'].apply(lambda x: any(item in x for item in filtre_categorie) if isinstance(x, list) else False)]
+
+    if ods_type_activite:
+        df = df[df['ods_type_activite'].isin(ods_type_activite)]
+
+    if nom_etablissement:
+        df = df[df['app_libelle_etablissement'].str.contains(nom_etablissement, case=False)]
+
+    if adresse:
+        df = df[df['adresse_2_ua'].str.contains(adresse, case=False)]
+
+    # Afficher la carte interactive
+    st.subheader('Carte des établissements')
+    map = folium.Map(location=[46.2276, 2.2137], zoom_start=6)
     for _, row in df.iterrows():
         if row['geores'] is not None:
-            latitude = row['geores']['lat']
-            longitude = row['geores']['lon']
-            tooltip = row['app_libelle_etablissement']
             folium.Marker(
-                location=[latitude, longitude],
+                location=[row['geores']['lat'], row['geores']['lon']],
                 popup=row['app_libelle_etablissement'],
-                tooltip=tooltip,
+                tooltip=row['app_libelle_etablissement'],
                 icon=folium.Icon(
                     color='green' if row['synthese_eval_sanit'] == 'Très satisfaisant' else 
                           'orange' if row['synthese_eval_sanit'] == 'Satisfaisant' else 
@@ -63,83 +118,11 @@ def create_map(df):
                 )
             ).add_to(map)
 
-    return map
-
-# Interface utilisateur Streamlit
-st.title('Données AlimConfiance')
-
-# Récupérer les données de l'API avec indicateur de chargement
-with st.spinner('Chargement des données...'):
-    year = st.sidebar.selectbox("Année d'inspection", [2024, 2023])
-    month = st.sidebar.selectbox("Mois d'inspection", range(1, 13), format_func=lambda x: f"{x:02}")
-    df = get_data_from_api(year, month)
-
-if df is not None:
-    # Filtrage
-    st.sidebar.title('Filtrage')
-
-    # Niveau de résultat
-    all_levels = ['Tous', 'Très satisfaisant', 'Satisfaisant', 'A améliorer', 'A corriger de manière urgente']
-    niveau_resultat = st.sidebar.selectbox("Niveau de résultat", all_levels)
-
-    # Activité de l'établissement
-    activite_etablissement_unique = set()
-    for activites in df['app_libelle_activite_etablissement']:
-        activite_etablissement_unique.update(activites)
-    activite_etablissement = st.sidebar.multiselect(
-        "Activité de l'établissement", sorted(activite_etablissement_unique)
-    )
-
-    # Catégorie de filtre
-    filtre_categorie_unique = set()
-    for filtres in df['filtre']:
-        if isinstance(filtres, list):
-            filtre_categorie_unique.update(filtres)
-    filtre_categorie = st.sidebar.multiselect(
-        "Catégorie de filtre", sorted(filtre_categorie_unique)
-    )
-
-    # Type d'activité
-    ods_type_activite = st.sidebar.multiselect(
-        "Type d'activité", sorted(df['ods_type_activite'].unique())
-    )
-
-    # Recherche par nom et adresse
-    nom_etablissement = st.sidebar.text_input("Nom de l'établissement")
-    adresse = st.sidebar.text_input("Adresse")
-
-    # Appliquer les filtres
-    if niveau_resultat != 'Tous':
-        df = df[df['synthese_eval_sanit'] == niveau_resultat]
-    
-    if activite_etablissement:
-        df = df[df['app_libelle_activite_etablissement'].apply(lambda x: any(item in x for item in activite_etablissement))]
-    
-    if filtre_categorie:
-        df = df[df['filtre'].apply(lambda x: any(item in x for item in filtre_categorie) if isinstance(x, list) else False)]
-    
-    if ods_type_activite:
-        df = df[df['ods_type_activite'].isin(ods_type_activite)]
-    
-    if nom_etablissement:
-        df = df[df['app_libelle_etablissement'].str.contains(nom_etablissement, case=False)]
-    
-    if adresse:
-        df = df[df['adresse_2_ua'].str.contains(adresse, case=False)]
-
-    # Afficher la carte interactive avec Streamlit-Folium
-    st.subheader('Carte des établissements')
-    map = create_map(df)
     folium_static(map, width=700, height=500)
 
-    # Afficher les informations détaillées avec un graphique interactif
+    # Afficher les informations détaillées
     st.subheader('Données détaillées')
     st.write(df)
-
-    # Visualisation des résultats par niveau de satisfaction
-    st.subheader('Répartition des évaluations sanitaires')
-    fig = px.pie(df, names='synthese_eval_sanit', title='Répartition des évaluations sanitaires')
-    st.plotly_chart(fig)
 
     # Permettre de télécharger les données filtrées
     csv = df.to_csv(index=False)
