cd88e20c0a597b8be12f1d04a2356707a087508e
MOON <133333114+M00N69@users.noreply.github.com>
2024-09-01 16:35:01 +0200

    Update ALIMCONF.py

--- ALIMCONF.py
+++ ALIMCONF.py
@@ -67,84 +67,104 @@ selected_end = pd.to_datetime(selected_end).tz_localize('UTC') + relativedelta(m
 # Filtrer les données en fonction de la plage de dates sélectionnée
 df_filtered = df[(df['Date_inspection'] >= selected_start) & (df['Date_inspection'] <= selected_end)]
 
-# Filtres pour Synthese_eval_sanit et APP_Libelle_activite_etablissement
-synthese_options = ['A corriger de manière urgente', 'Tous'] + list(df_filtered['Synthese_eval_sanit'].unique())
-selected_synthese = st.selectbox("Filtrer par Synthèse d'évaluation sanitaire", synthese_options, index=0)
+# Vérifier l'existence des colonnes nécessaires
+required_columns = [
+    'APP_Libelle_activite_etablissement', 
+    'APP_Libelle_etablissement', 
+    'Code_postal', 
+    'com_name', 
+    'Synthese_eval_sanit', 
+    'Date_inspection', 
+    'geores', 
+    'Agrement', 
+    'reg_name', 
+    'dep_name', 
+    'filtre'
+]
 
-# Filtre pour la colonne 'filtre'
-filtre_options = ['Tous'] + sorted(df_filtered['filtre'].unique().tolist())
-selected_filtre = st.selectbox("Filtrer par type de contrôle", filtre_options)
-
-activite_options = ['Tous'] + list(df_filtered['APP_Libelle_activite_etablissement'].unique())
-selected_activite = st.selectbox("Filtrer par Activité de l'établissement", activite_options)
-
-if selected_synthese != 'Tous':
-    df_filtered = df_filtered[df_filtered['Synthese_eval_sanit'] == selected_synthese]
-
-if selected_filtre != 'Tous':
-    df_filtered = df_filtered[df_filtered['filtre'] == selected_filtre]
-
-if selected_activite != 'Tous':
-    df_filtered = df_filtered[df_filtered['APP_Libelle_activite_etablissement'] == selected_activite]
-
-# Afficher la carte interactive
-st.subheader('Carte des établissements')
-map = create_map(df_filtered)
-st.components.v1.html(map._repr_html_(), width=1200, height=600)
-
-# Afficher les informations détaillées
-st.subheader('Données détaillées des établissements')
-st.dataframe(df_filtered)
-
-# Permettre de télécharger les données filtrées
-csv = df_filtered.to_csv(index=False)
-st.download_button("Télécharger les données", csv, file_name="data_filtered.csv", mime="text/csv")
-
-# Afficher la fiche complète d'un site sélectionné avec un meilleur design
-if not df_filtered.empty:
-    selected_site = st.selectbox("Sélectionner un site", df_filtered['APP_Libelle_etablissement'])
-    selected_site_data = df_filtered[df_filtered['APP_Libelle_etablissement'] == selected_site].iloc[0]
-    st.subheader('Fiche complète du site sélectionné')
-    
-    # CSS pour un design attrayant et professionnel
-    st.markdown("""
-    <style>
-    .site-info {
-        background-color: #f0f2f6;
-        border-radius: 10px;
-        padding: 20px;
-        margin-bottom: 20px;
-    }
-    .site-info h3 {
-        color: #0e1117;
-        border-bottom: 2px solid #0e1117;
-        padding-bottom: 10px;
-        margin-bottom: 20px;
-    }
-    .site-info p {
-        margin-bottom: 10px;
-    }
-    .site-info strong {
-        color: #0e1117;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
-    # Fonction pour obtenir une valeur sûre
-    def safe_get(data, key, default="Non spécifié"):
-        return data[key] if key in data and pd.notna(data[key]) else default
-
-    # Affichage des informations du site
-    st.markdown(f"""
-    <div class="site-info">
-        <h3>{safe_get(selected_site_data, 'APP_Libelle_etablissement')}</h3>
-        <p><strong>Adresse:</strong> {safe_get(selected_site_data, 'Adresse_2_UA')}</p>
-        <p><strong>Code postal:</strong> {safe_get(selected_site_data, 'Code_postal_UA')}</p>
-        <p><strong>Commune:</strong> {safe_get(selected_site_data, 'Libelle_commune_UA')}</p>
-        <p><strong>Activité:</strong> {safe_get(selected_site_data, 'APP_Libelle_activite_etablissement')}</p>
-        <p><strong>Date d'inspection:</strong> {selected_site_data['Date_inspection'].strftime('%d/%m/%Y')}</p>
-        <p><strong>Synthèse de l'évaluation sanitaire:</strong> {safe_get(selected_site_data, 'Synthese_eval_sanit')}</p>
-    </div>
-    """, unsafe_allow_html=True)
+# Check for missing columns
+missing_columns = [col for col in required_columns if col not in df_filtered.columns]
+if missing_columns:
+    st.error(f"Les colonnes suivantes sont manquantes dans les données: {', '.join(missing_columns)}")
 else:
-    st.write("Aucun établissement trouvé dans la période sélectionnée.")
+    # Filtres pour Synthese_eval_sanit et APP_Libelle_activite_etablissement
+    synthese_options = ['A corriger de manière urgente', 'Tous'] + list(df_filtered['Synthese_eval_sanit'].unique())
+    selected_synthese = st.selectbox("Filtrer par Synthèse d'évaluation sanitaire", synthese_options, index=0)
+
+    # Filtre pour la colonne 'filtre'
+    filtre_options = ['Tous'] + sorted(df_filtered['filtre'].dropna().unique().tolist())
+    selected_filtre = st.selectbox("Filtrer par type de contrôle", filtre_options)
+
+    activite_options = ['Tous'] + list(df_filtered['APP_Libelle_activite_etablissement'].dropna().unique())
+    selected_activite = st.selectbox("Filtrer par Activité de l'établissement", activite_options)
+
+    if selected_synthese != 'Tous':
+        df_filtered = df_filtered[df_filtered['Synthese_eval_sanit'] == selected_synthese]
+
+    if selected_filtre != 'Tous':
+        df_filtered = df_filtered[df_filtered['filtre'] == selected_filtre]
+
+    if selected_activite != 'Tous':
+        df_filtered = df_filtered[df_filtered['APP_Libelle_activite_etablissement'] == selected_activite]
+
+    # Afficher la carte interactive
+    st.subheader('Carte des établissements')
+    map = create_map(df_filtered)
+    st.components.v1.html(map._repr_html_(), width=1200, height=600)
+
+    # Afficher les informations détaillées
+    st.subheader('Données détaillées des établissements')
+    st.dataframe(df_filtered)
+
+    # Permettre de télécharger les données filtrées
+    csv = df_filtered.to_csv(index=False)
+    st.download_button("Télécharger les données", csv, file_name="data_filtered.csv", mime="text/csv")
+
+    # Afficher la fiche complète d'un site sélectionné avec un meilleur design
+    if not df_filtered.empty:
+        selected_site = st.selectbox("Sélectionner un site", df_filtered['APP_Libelle_etablissement'])
+        selected_site_data = df_filtered[df_filtered['APP_Libelle_etablissement'] == selected_site].iloc[0]
+        st.subheader('Fiche complète du site sélectionné')
+
+        # CSS pour un design attrayant et professionnel
+        st.markdown("""
+        <style>
+        .site-info {
+            background-color: #f0f2f6;
+            border-radius: 10px;
+            padding: 20px;
+            margin-bottom: 20px;
+        }
+        .site-info h3 {
+            color: #0e1117;
+            border-bottom: 2px solid #0e1117;
+            padding-bottom: 10px;
+            margin-bottom: 20px;
+        }
+        .site-info p {
+            margin-bottom: 10px;
+        }
+        .site-info strong {
+            color: #0e1117;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+        # Fonction pour obtenir une valeur sûre
+        def safe_get(data, key, default="Non spécifié"):
+            return data[key] if key in data and pd.notna(data[key]) else default
+
+        # Affichage des informations du site
+        st.markdown(f"""
+        <div class="site-info">
+            <h3>{safe_get(selected_site_data, 'APP_Libelle_etablissement')}</h3>
+            <p><strong>Adresse:</strong> {safe_get(selected_site_data, 'Adresse_2_UA')}</p>
+            <p><strong>Code postal:</strong> {safe_get(selected_site_data, 'Code_postal')}</p>
+            <p><strong>Commune:</strong> {safe_get(selected_site_data, 'com_name')}</p>
+            <p><strong>Activité:</strong> {safe_get(selected_site_data, 'APP_Libelle_activite_etablissement')}</p>
+            <p><strong>Date d'inspection:</strong> {selected_site_data['Date_inspection'].strftime('%d/%m/%Y')}</p>
+            <p><strong>Synthèse de l'évaluation sanitaire:</strong> {safe_get(selected_site_data, 'Synthese_eval_sanit')}</p>
+        </div>
+        """, unsafe_allow_html=True)
+    else:
+        st.write("Aucun établissement trouvé dans la période sélectionnée.")
