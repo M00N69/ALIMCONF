7d9cf4651827a3c1a48ceee9e66c8dd18e835395
MOON <133333114+M00N69@users.noreply.github.com>
2024-09-01 17:34:04 +0200

    Update ALIMCONF.py

--- ALIMCONF.py
+++ ALIMCONF.py
@@ -205,4 +205,16 @@ elif page == "Statistiques":
 
     st.subheader('Évaluations mensuelles')
     st.pyplot(fig3)
+    
+# --- Logo and Link in Sidebar ---
+    st.sidebar.markdown(
+        f"""
+        <div class="sidebar-logo-container">
+            <a href="https://www.visipilot.com" target="_blank">
+                <img src="https://raw.githubusercontent.com/M00N69/RAPPELCONSO/main/logo%2004%20copie.jpg" alt="Visipilot Logo" class="sidebar-logo">
+            </a>
+        </div>
+        """, unsafe_allow_html=True
+    )
+
 
